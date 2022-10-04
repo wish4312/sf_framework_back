7b951e9530f9d5a55e5976f8a29d76614071e999
김승현 <shkim@lsitc.com>
2022-10-05 05:53:35 +0900

    기능: [#59] 공통 Mybatis Interceptor 구현
    공통 Mybatis Interceptor에 맞춰 Sample 수정
    
    close: #59

--- src/main/java/com/lsitc/global/mybatis/DefaultInterceptor.java
+++ src/main/java/com/lsitc/global/mybatis/DefaultInterceptor.java
@@ -1,5 +1,10 @@
 package com.lsitc.global.mybatis;
 
+import com.lsitc.global.auditing.Auditable;
+import com.lsitc.global.auditing.CurrentDateTimeProvider;
+import com.lsitc.global.auditing.CurrentUserInfoProvider;
+import com.lsitc.global.auditing.DateTimeProvider;
+import com.lsitc.global.auditing.UserProvider;
 import com.lsitc.global.common.BaseVo;
 import com.lsitc.global.common.SessionVo;
 import com.lsitc.global.util.JwtTokenUtils;
@@ -9,6 +14,7 @@ import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
@@ -19,13 +25,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Intercepts({
-    @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class,
-        RowBounds.class, ResultHandler.class, CacheKey.class,
-        BoundSql.class}),
-    @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class,
-        RowBounds.class, ResultHandler.class}),
-    @Signature(type = Executor.class, method = "update", args = {MappedStatement.class,
-        Object.class})
+    @Signature(type = Executor.class, method = "query",
+        args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class,
+            CacheKey.class, BoundSql.class}),
+    @Signature(type = Executor.class, method = "query",
+        args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+    @Signature(type = Executor.class, method = "update",
+        args = {MappedStatement.class, Object.class})
 })
 public class DefaultInterceptor implements Interceptor {
 
@@ -38,6 +44,9 @@ public class DefaultInterceptor implements Interceptor {
    * @desc : mybatis에서 sql 수행 시 수행되는 interceptor로, session객체를 추가해준다. (참조 ::
    * https://developpaper.com/mybatis-interceptor/)
    */
+  private DateTimeProvider dateTimeProvider = CurrentDateTimeProvider.INSTANCE;
+  private UserProvider userProvider = CurrentUserInfoProvider.INSTANCE;
+
   @SuppressWarnings("unchecked")
   @Override
   public Object intercept(Invocation invocation) throws Throwable {
@@ -47,7 +56,21 @@ public class DefaultInterceptor implements Interceptor {
     //세션객체
     SessionVo sessionVo = JwtTokenUtils.getSessionVo();
 
-    if (parameter instanceof Map || parameter instanceof HashMap) {
+    if (parameter instanceof Auditable) {
+      Auditable baseAbstractEntity = (Auditable) parameter;
+      if ("update".equals(invocation.getMethod().getName())) {
+        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+        if (mappedStatement.getSqlCommandType().equals(SqlCommandType.INSERT)) {
+          baseAbstractEntity.setCreatedBy(userProvider.getId());
+          baseAbstractEntity.setCreatedDate(dateTimeProvider.getNow());
+          baseAbstractEntity.setLastModifiedBy(userProvider.getId());
+          baseAbstractEntity.setLastModifiedDate(dateTimeProvider.getNow());
+        } else if (mappedStatement.getSqlCommandType().equals(SqlCommandType.UPDATE)) {
+          baseAbstractEntity.setLastModifiedBy(userProvider.getId());
+          baseAbstractEntity.setLastModifiedDate(dateTimeProvider.getNow());
+        }
+      }
+    } else if (parameter instanceof Map || parameter instanceof HashMap) {
       //map일때
       Map<String, Object> map = (Map<String, Object>) parameter;
       map.put("session", sessionVo);
