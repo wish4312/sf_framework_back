00cd2bad5f50854965e12dec4535597b8d41fcef
김승현 <shkim@lsitc.com>
2022-10-28 10:49:16 +0900

    기능: [#98] 가이드용 POST 단건/다건 통합 메소드 샘플소스개발
    close: #98

--- src/main/java/com/lsitc/domain/sample/controller/SampleController.java
+++ src/main/java/com/lsitc/domain/sample/controller/SampleController.java
@@ -59,9 +59,10 @@ public class SampleController {
 
   @PostMapping
   public SampleAddResponseVO addSample(
-      @RequestBody @Valid final SampleAddRequestVO sampleAddRequestVO) throws SampleException {
+      @RequestBody @Valid final List<SampleAddRequestVO> sampleAddRequestVO)
+      throws SampleException {
     log.info("post 메소드가 호출되었습니다.");
-    if ("ERROR".equals(sampleAddRequestVO.getFoo())) {
+    if ("ERROR".equals(sampleAddRequestVO.get(0).getFoo())) {
       throw new SampleException("오류발생", ErrorCode.INTERNAL_SERVER_ERROR);
     }
     log.info(sampleAddRequestVO.toString());
