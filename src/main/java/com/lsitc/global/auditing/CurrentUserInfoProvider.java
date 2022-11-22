9366b5db4dccef45626cc715bf6c6573cd586b4b
오운영 <uyoh@lsitc.com>
2022-11-22 17:06:53 +0900

    리팩토링: CurrentUserInfoProvider에 사용되던 Map을 Entity로 변경

--- src/main/java/com/lsitc/global/auditing/CurrentUserInfoProvider.java
+++ src/main/java/com/lsitc/global/auditing/CurrentUserInfoProvider.java
@@ -1,33 +1,30 @@
 package com.lsitc.global.auditing;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.security.core.context.SecurityContextHolder;
+import com.lsitc.domain.common.user.entity.UserEntity;
 
-public enum CurrentUserInfoProvider implements UserProvider<Map<String, String>, Long> {
+public enum CurrentUserInfoProvider implements UserProvider<UserEntity, Long> {
   INSTANCE;
 
   @Override
-  public Map<String, String> getUser() {
+  public UserEntity getUser() {
     Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if (principal instanceof Map) {
-      return (Map<String, String>) principal;
+    if (principal instanceof UserEntity) {
+      UserEntity userEntity = (UserEntity) principal;
+      return userEntity;
     } else {
-      return new HashMap<String, String>() {{
-        put("userNo", null);
-        put("userNm", "anonymous");
-      }};
+      return UserEntity.builder().id(0L).name("anonymous").build();
     }
   }
 
   @Override
   public Long getId() {
     Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if (principal instanceof Map) {
-      Map<String, String> userInfo = (Map<String, String>) principal;
-      return Long.valueOf(userInfo.get("userNo"));
+    if (principal instanceof UserEntity) {
+      UserEntity userEntity = (UserEntity) principal;
+      return userEntity.getId();
     } else {
-      return null;
+      return 0L;
     }
   }
 }
